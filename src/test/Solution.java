f1a2f67a1d0f05a70ed538c277a7bc09a2b18743
Nguyen Van Vuong <vuongluis@hotmail.com>
2016-07-06 09:22:19 +0700

    ok

---
+++ src/test/Solution.java
@@ -0,0 +1,16 @@
+package test;
+
+@SuppressWarnings("all")
+public class Solution {
+	public int solution(int N){
+		String result = "-1";
+		do{
+			result += String.valueOf(N%2);
+			N = N/2;
+		}while(N/2 != 0);
+		if("-1".e)
+	}
+	public static void main(String[] args) {
+		System.out.println("Kết quả: "+new Solution().solution(4));
+	}
+}
